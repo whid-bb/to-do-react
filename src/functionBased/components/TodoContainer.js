aee6dc91cd2c6291d98e7fb39a0ce9c0216d51a2
whid-bb <whid11s50@gmail.com>
2022-07-05 17:56:12 +0200

    add localStorage

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
 import { v4 as uuidv4 } from "uuid";
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(getInitialTodos());
 
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem("todos", temp);
+  }, [todos]);
+
+  function getInitialTodos() {
+    const temp = localStorage.getItem("todos");
+    const savedTodos = JSON.parse(temp);
+
+    return savedTodos || [];
+  };
 
   const handleChange = id => {
     setTodos(prevState =>
@@ -50,6 +61,7 @@ const TodoContainer = () => {
     })
     );
   };
+
 
 
   return (
